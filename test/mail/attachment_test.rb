74313117072d7c6820160a21258b2f72d7abe6b9
Ben Jarman <benjarman@Bens-MacBook-Pro-8.local>
2018-04-02 10:20:02 -0700

    added some asserts

--- test/mail/attachment_test.rb
+++ test/mail/attachment_test.rb
@@ -3,6 +3,7 @@ require 'fakefs'
 include FakeFS
 require_relative '../../lib/mail/attachment'
 
+# TODO add better code coverage
 class AttachmentTest < Minitest::Test
   def setup
     FakeFS.activate!
@@ -14,9 +15,9 @@ class AttachmentTest < Minitest::Test
   end
 
   def test_attachment_writes_to_file
-    puts 'in the test case'
     a = Attachment.new("sometextfile.txt", "blablabla", false)
     result_path = a.write("ben@example.com")
-    puts result_path
+    assert result_path.include? '283c17ae3eae66fca8cdd3a5cd16ff78e0bb150b'
+    assert result_path.include? '/content/'
   end
 end
