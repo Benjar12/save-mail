127beedcd8484db294f972ff9085fe01b124d23d
Ben Jarman <benjarman@Bens-MacBook-Pro-8.local>
2018-04-02 10:04:20 -0700

    got the basics of this class working

--- lib/mail/attachment.rb
+++ lib/mail/attachment.rb
@@ -2,18 +2,22 @@ require_relative '../fs_util/fs'
 include FSUtil
 
 class Attachment
+  @@filename
+  @@payload
+  @@is_body
+
   def initialize(filename, payload, is_body)
-    @filename = filename
-    @payload = payload
-    @is_body = is_body
+    @@filename = filename
+    @@payload = payload
+    @@is_body = is_body
   end
 
   def set_path(file_path)
-    @file_path = file_path
+    @@file_path = file_path
   end
 
   def write(email_address)
-    path = FSUtil.create_and_write(email_address, self.filename, self.payload, false)
+    path = FSUtil.create_and_write(email_address, @@filename, @@payload, false)
     self.set_path(path)
   end
 end
